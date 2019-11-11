56e425a0ad3fd8bd655fbae1ff3bce52316edca5
Rizky Yoga Oktora <Yoga_RYO495@mitrais.com>
2019-11-11 14:13:32 +0700

    fixing profiling for dev and prod to use it properly

--- app/src/main/java/com/yoga/atm/app/auth/CustomAuthenticationProvider.java
+++ app/src/main/java/com/yoga/atm/app/auth/CustomAuthenticationProvider.java
@@ -1,7 +1,6 @@
 package com.yoga.atm.app.auth;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,14 +9,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
-import com.yoga.atm.app.dao.AccountRepository;
 import com.yoga.atm.app.model.Account;
+import com.yoga.atm.app.service.AccountService;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 	@Autowired
-	private AccountRepository accountService;
+	private AccountService accountService;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -33,11 +32,10 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		} else if (!pin.matches("[0-9]+")) {
 			return null;
 		}
-		List<Account> listAccount = accountService.findByAccountNumberAndPin(accountNumber, pin);
-		if (listAccount.size() <= 0) {
+		Account account = accountService.validate(accountNumber, pin);
+		if (account == null) {
 			return null;
 		} else {
-			Account account = listAccount.get(0);
 			return new UsernamePasswordAuthenticationToken(account, null, new ArrayList<>());
 		}
 	}
--- app/src/main/java/com/yoga/atm/app/config/SpringSecurityConfig.java
+++ app/src/main/java/com/yoga/atm/app/config/SpringSecurityConfig.java
@@ -43,7 +43,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/plugins/**");
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/plugins/**","/fonts/**");
 	}
 
 	@Bean
